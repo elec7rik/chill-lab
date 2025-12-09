6fb5f469984b372b9c5519f0c3d11eac21480e49
elec7rik <vickyvirus258@gmail.com>
2025-12-10 00:12:46 +0530

    day5-7: basic CRUD functionality complete

--- day5-7/react-supabase/src/App.tsx
+++ day5-7/react-supabase/src/App.tsx
@@ -1,19 +1,94 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { supabase } from "./supabase-client";
+
+interface Task {
+  id: number;
+  title: string;
+  description: string;
+  created_at: string;
+}
 
 function App() {
+  const [newTask, setNewTask] = useState({title : "", description: ""});
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [newDesciption, setNewDescription] = useState("");
+
+  const fetchTasks = async () => {
+    const { error, data } = await supabase
+      .from("tasks")
+      .select("*")
+      .order("created_at", { ascending: true });
+
+    if(error) {
+      console.error("Error reading task: ", error.message);
+      return;
+    }
+
+    setTasks(data);
+  }
+
+  const updateTask = async (id: number) => {
+    const { error } = await supabase
+      .from("tasks")
+      .update({ description: newDesciption })
+      .eq("id", id);
+
+    if ( error ) {
+      console.error(`error updating task: `, error.message);
+      return;
+    }
+  }
+
+  const deleteTask = async (id: number) => {
+    const { error } = await supabase.from("tasks").delete().eq("id", id);
+
+    if ( error ) {
+      console.error(`error deleting task: `, error.message);
+      return;
+    }
+  };
+  
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    const { error } = await supabase.from("tasks").insert(newTask).single();
+
+    if ( error ) {
+      console.error(`error adding task: `, error.message);
+      return;
+    }
+
+    setNewTask({ title: "", description: "" });
+  };
+
+  useEffect(() => {
+    fetchTasks()
+  }, []);
+
+  console.log(tasks);
+  
   return (
     <div style={{ maxWidth: "600px", margin: "0 auto", padding: "1rem" }}>
       <h2>Task Manager CRUD</h2>
 
       {/* Form to add a new task */}
-      <form style={{ marginBottom: "1rem" }}>
+      <form onSubmit={handleSubmit} style={{ marginBottom: "1rem" }}>
         <input
           type="text"
           placeholder="Task Title"
+          // will execute when the title text is changed
+          onChange={(e) => 
+            setNewTask((prev) => ({...prev, title: e.target.value}))
+          }
           style={{ width: "100%", marginBottom: "0.5rem", padding: "0.5rem" }}
         />
         <textarea
           placeholder="Task Description"
+          // will execute when the description text is changed
+          onChange={(e) => 
+            setNewTask((prev) => ({...prev, description: e.target.value}))
+          }
           style={{ width: "100%", marginBottom: "0.5rem", padding: "0.5rem" }}
         />
         <button type="submit" style={{ padding: "0.5rem 1rem" }}>
@@ -23,7 +98,9 @@ function App() {
 
       {/* List of Tasks */}
       <ul style={{ listStyle: "none", padding: 0 }}>
+        {tasks.map((task, key) => (
         <li
+          key={key}
           style={{
             border: "1px solid #ccc",
             borderRadius: "4px",
@@ -32,16 +109,26 @@ function App() {
           }}
         >
           <div>
-            <h3>Title</h3>
-            <p>Description</p>
+            <h3>{task.title}</h3>
+            <p>{task.description}</p>
             <div>
-              <button style={{ padding: "0.5rem 1rem", marginRight: "0.5rem" }}>
+              <textarea 
+                placeholder="Updated description..." 
+                onChange={(e) => setNewDescription(e.target.value)}
+              />
+              <button 
+                style={{ padding: "0.5rem 1rem", marginRight: "0.5rem" }}
+                onClick={() => updateTask(task.id)}
+              >
                 Edit
               </button>
-              <button style={{ padding: "0.5rem 1rem" }}>Delete</button>
+              <button style={{ padding: "0.5rem 1rem" }} 
+              onClick={() => deleteTask(task.id)}
+              >
+                Delete</button>
             </div>
           </div>
-        </li>
+        </li>))}
       </ul>
     </div>
   );
