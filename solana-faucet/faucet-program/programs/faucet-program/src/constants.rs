a9a9e2da2b5b06dcdfa1a72cad93b988c6b58df0
elec7rik <vickyvirus258@gmail.com>
2025-12-19 18:53:54 +0530

    write basic tests; vault initializationt test

--- solana-faucet/faucet-program/programs/faucet-program/src/constants.rs
+++ solana-faucet/faucet-program/programs/faucet-program/src/constants.rs
@@ -1,6 +1,6 @@
 // faucet config
 pub const LAMPORTS_PER_SOL: u64 = 1_000_000_000;
 
-pub const CLAIM_AMOUNT: u64 = 200_000_000; // 0.2 SOL per request
+pub const CLAIM_AMOUNT: u64 = 2_000_000; 
 pub const DAILY_LIMIT: u64 = 1 * LAMPORTS_PER_SOL; // 1 SOL/day/user
 pub const COOLDOWN_SECONDS: i64 = 180; // 3 minutes
--- solana-faucet/faucet-program/tests/faucet-program.ts
+++ solana-faucet/faucet-program/tests/faucet-program.ts
@@ -1,16 +1,36 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { FaucetProgram } from "../target/types/faucet_program";
+import { expect } from "chai";
 
 describe("faucet-program", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.faucetProgram as Program<FaucetProgram>;
+  const program = anchor.workspace.FaucetProgram as Program<FaucetProgram>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+  const user = provider.wallet;
+  const connection = provider.connection;
+
+  it("initializes the vault", async () => {
+  await program.methods
+    .initializeVault()
+    .accounts({
+      payer: user.publicKey,
+    })
+    .rpc();
+
+  const [vaultPda] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("vault")],
+    program.programId
+  );
+
+  const vaultInfo = await connection.getAccountInfo(vaultPda);
+
+  expect(vaultInfo).to.not.be.null;
+  expect(vaultInfo!.owner.toBase58())
+    .to.equal(program.programId.toBase58());
+});
+
+
 });
