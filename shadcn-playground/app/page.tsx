90acf7b4e69b505b9bfe92b42655b1a28b1d5551
elec7rik <vickyvirus258@gmail.com>
2026-01-03 11:46:26 +0530

    simple button example

--- shadcn-playground/app/page.tsx
+++ shadcn-playground/app/page.tsx
@@ -1,8 +1,28 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Button } from "@/components/ui/button"
+
 export default function DialogDemo() {
   return (
     <main className="min-h-screen flex items-center justify-center">
       <div className="max-w-5xl mx-auto p-16">
-        <p>dropdown-menu next</p>
+        <DropdownMenu>
+        <DropdownMenuTrigger asChild><Button>Open</Button></DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
       </div>
     
     </main>
